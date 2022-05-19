5a7037ad9e5b7c27b0c12e63f39791789dea407c
mabolek <mathias@lilio.com>
2022-05-19 14:24:34 +0200

    [TASK] Add correct icons

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'tx-oracle-logo' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:oracle_dam/Resources/Public/Icons/Oracle.svg',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -9,4 +9,18 @@ defined('TYPO3_MODE') or die('Access denied.');
         'priority' => 50,
         'class' => \Oracle\Typo3Dam\FormEngine\Container\SelectorButtonContainer::class,
     ];
+
+    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 11000000) {
+        return;
+    }
+
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Imaging\IconRegistry::class
+    );
+
+    $iconRegistry->registerIcon(
+        'tx-oracle-logo',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:oracle_dam/Resources/Public/Icons/Oracle.svg']
+    );
 })();
