01630b6b84c0d83994b1b8d710973dd152cf6fb7
Mathias Bolt Lesniak <mathias@lilio.com>
2022-05-27 14:58:29 +0200

    [FEATURE] Asset select UI for TYPO3 backend (#1)
    Select an asset from Oracle DAM in the TYPO3 backend. Does not yet download the asset and interact with the REST API on the server side, but inserts a hard-coded file ID on the server side as if a file was downloaded.

--- Resources/Public/JavaScript/SelectorLoader.js
+++ Resources/Public/JavaScript/SelectorLoader.js
@@ -8,8 +8,13 @@ define([
 ], function (NProgress, Modal, Severity, MessageUtility, AjaxRequest, DocumentService) {
   'use strict';
 
-  var SelectorPlugin = function (element) {
-    var self = this;
+  const SelectorPlugin = function (element) {
+    const self = this;
+
+    self.irreObjectId = element.dataset.fileIrreObject;
+    self.allowedExtensions = element.dataset.fileAllowed.split(',');
+
+    self.selectedAssets = [];
 
     self.openModal = function () {
       self.$modal = Modal.advanced({
@@ -18,33 +23,71 @@ define([
         content: '',
         severity: Severity.default,
         size: Modal.sizes.full,
+        buttons: [
+          {
+            text: TYPO3.lang['oracle_dam.modal.button.cancel'],
+            active: false,
+            trigger: function () {
+              self.selectedAssets = [];
+              self.$modal.modal('hide');
+            },
+            btnClass: 'btn-default'
+          },
+          {
+            text: TYPO3.lang['oracle_dam.modal.button.use-selected'],
+            active: false,
+            trigger: function () {
+              self.$modal.modal('hide');
+
+              self.addAssets(self.selectedAssets);
+            },
+            btnClass: 'btn-success'
+          }
+        ],
         callback: function (modal) {
           window.top.require([TYPO3.settings.oracle_dam.jsUiUrl], function () {
             window.top.OracleCEUI.oceUrl = 'https://' + TYPO3.settings.oracle_dam.oceDomain;
 
-            var frame = window.top.OracleCEUI.assetsView.createFrame({
-              assetsView: {
-                select: 'single',
-                filter: {
-                  bar: {
-                    capsules: false,
-                    relatedKeywords: true,
+            const frame = window.top.OracleCEUI.assetsView.createFrame(
+              {
+                assetsView: {
+                  select: 'multiple',
+                  filter: {
+                    bar: {
+                      capsules: false,
+                      relatedKeywords: true,
+                    },
+                    repositories: ['none'],
+                    channels: [TYPO3.settings.oracle_dam.channelId],
+                    mediaGroups: ['images'],
+                    assetStatus: ['published'],
                   },
-                  repositories: ['none'],
-                  channels: [],
-                  mediaGroups: ['images'],
-                  assetStatus: ['published'],
-                },
-                filterValue: {
-                  repositoryId: TYPO3.settings.oracle_dam.repositoryId,
-                  channelId: TYPO3.settings.oracle_dam.channelId,
-                  mediaGroups: ['images'],
-                  assetStatus: ['published'],
+                  filterValue: {
+                    repositoryId: TYPO3.settings.oracle_dam.repositoryId,
+                    channelId: TYPO3.settings.oracle_dam.channelId,
+                    mediaGroups: ['images'],
+                    assetStatus: ['published'],
+                  },
                 },
               },
-            });
+              {
+                selectionChanged: async function (frame, selection) {
+                  self.selectedAssets = selection;
 
-            var $modalBody = $('.modal-body', modal);
+                  var $successButton = $('.modal-footer .btn-success', self.$modal).first();
+
+                  if (selection.length === 0) {
+                    $successButton.attr('disabled', true);
+
+                    return;
+                  }
+
+                  $successButton.attr('disabled', false);
+                }
+              }
+            );
+
+            const $modalBody = $('.modal-body', modal);
 
             $(frame)
               .css('height', '100%')
@@ -57,6 +100,96 @@ define([
               .appendChild(frame);
           });
         },
+      });
+    };
+
+    self.addAssets = function (assets) {
+      NProgress.start();
+
+      const request = new AjaxRequest(TYPO3.settings.ajaxUrls['oracle_dam_download_file']);
+
+      const assetIds = [];
+
+      for (let asset of assets) {
+        if (!self.validateAsset(asset)) {
+          return;
+        }
+
+        assetIds.push(asset.id);
+      }
+
+      request.post({
+        assets: assetIds.join(',')
+      }).then(
+        async function (response) {
+          const data = await response.resolve();
+
+          if (response.response.status !== 200 || !data.success) {
+            let errorMessage = TYPO3.lang['oracle_dam.modal.request-failed'];
+
+            if (data.message) {
+              errorMessage = data.message;
+            }
+
+            self.displayError(errorMessage);
+
+            NProgress.done();
+
+            return;
+          }
+
+          for (let i = 0; i < data.fileUids.length; i++) {
+            MessageUtility.MessageUtility.send({
+              actionName: 'typo3:foreignRelation:insert',
+              objectGroup: self.irreObjectId,
+              table: 'sys_file',
+              uid: data.fileUids[i]
+            });
+          }
+
+          NProgress.done();
+        },
+        function (error) {
+          self.displayError(TYPO3.lang['oracle_dam.modal.request-failed'] + error.status + ' ' + error.statusText);
+
+          NProgress.done();
+        }
+      );
+    };
+
+    /**
+     * Returns true if the asset can be inserted.
+     *
+     * @param asset from Oracle
+     * @returns {boolean}
+     */
+    self.validateAsset = function (asset) {
+      if (self.allowedExtensions.indexOf(asset.fileExtension) === -1) {
+        self.displayError(TYPO3.lang['oracle_dam.modal.illegal-extension'].replace('{0}', asset.fileExtension));
+        return false;
+      }
+
+      return true;
+    }
+
+    /**
+     * Displays an error message in a modal.
+     *
+     * @param message The error message to display
+     */
+    self.displayError = function (message) {
+      const errorModal = Modal.confirm(
+        TYPO3.lang['oracle_dam.modal.error-title'],
+        message,
+        Severity.error,
+        [{
+          text: TYPO3.lang['button.ok'] || 'OK',
+          btnClass: 'btn-' + Severity.getCssClass(Severity.error),
+          name: 'ok',
+          active: true,
+        }]
+      ).on('confirm.button.ok', function () {
+        errorModal.modal('hide');
       });
     };
   };
